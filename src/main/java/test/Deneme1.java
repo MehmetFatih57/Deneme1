3673495cf34be5fd5873ea84490cbc3000a4a6a4
Mehmet Fatih YILDIRIM <mehmet.fatihh57@gmail.com>
2023-04-24 20:54:46 +0300

    Sevgi

--- src/main/java/test/Deneme1.java
+++ src/main/java/test/Deneme1.java
@@ -3,5 +3,7 @@ package test;
 public class Deneme1 {
     public static void main(String[] args) {
         System.out.println("GitHub Deneme");
+        System.out.println("GitHub'dan sevgilerle");
+
     }
 }
